755cd49c1480acefd5ec21434e07a15053903566
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-23 23:29:13 +0200

    feat: initial structure

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
+
+//middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+console.log("userRoute: ", userRoute);
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+// app.get("/", (req, res) => {
+//     res.send("welcome to homepage");
+// })
+// app.get("/users", (req, res) => {
+//     res.send("welcome to user page");
+// })
+
+app.listen(8800, () => {
+    console.log("Backend server is running! said that Tugce");
+})
