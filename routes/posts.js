7309e633373ea3669a533a41c46b4ba631c86959
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-24 23:21:28 +0200

    feat: create model and route for the posts and update index

---
+++ routes/posts.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+
+//Create a post
+
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//Update a post
+//Delete a post
+//Like a post
+//Get a post
+//Get timeline posts
+
+module.exports = router;
