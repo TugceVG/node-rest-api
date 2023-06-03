e898fbd295cb1e2e6ce07e49e1d826fb021b56d8
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-06-03 23:08:17 +0200

    feat: add new dependency for local files

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+const multer = require('multer');
 
 dotenv.config();
 
@@ -17,6 +18,24 @@ mongoose.connect(process.env.MONGO_URL);
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("file uploaded successfully");
+    } catch (err) {
+        console.log(err);
+    }
+})
 
 console.log("userRoute: ", userRoute);
 
